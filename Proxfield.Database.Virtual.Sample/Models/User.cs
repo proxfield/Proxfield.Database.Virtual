e30b4703f17f7396d0eb2a0660c6c8945564998e
Marco Lima <marckdx@outlook.com>
2022-09-22 22:45:25 -0300

    removing old project

--- Proxfield.Database.Virtual.Sample/Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace Proxfield.Database.Virtual.Sample.Models
-{
-    public class User : BaseEntity
-    {
-        public override string? Id {get; set;}
-        public string? Name {get; set;}
-    }
-}
